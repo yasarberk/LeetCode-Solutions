c74686188d691c88a93cf8e524ac47242023b2df
Berk Yaşar <93190355+yasarberk@users.noreply.github.com>
2023-08-17 13:25:45 +0300

    Create 167-Two-Sum-II-Input-Array-Is-Sorted.py

---
+++ 167-Two-Sum-II-Input-Array-Is-Sorted.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        l, r = 0, len(numbers)-1
+
+        while l < r:
+            currentSum = numbers[l] + numbers[r]
+
+            if (currentSum < target):
+                l += 1
+            elif (currentSum > target):
+                r -= 1
+            else:
+                return [l+1, r+1]
